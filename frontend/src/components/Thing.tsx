9172157e9a13c051b3bd3891250ecc594862a376
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-11-13 11:21:17 +0100

    Implemented some frontend components

---
+++ frontend/src/components/Thing.tsx
@@ -0,0 +1,5 @@
+const Thing = () => {
+    return <button>This is a button</button>
+}
+
+export default Thing
---
+++ frontend/src/components/ThingGroup.tsx
@@ -0,0 +1,23 @@
+import Thing from "./thing";
+
+const ThingGroup = () => {
+  return (
+    <div>
+      <div>
+        <Thing />
+        <Thing />
+        <Thing />
+        <Thing />
+      </div>
+
+      <div>
+        <Thing />
+        <Thing />
+        <Thing />
+        <Thing />
+      </div>
+    </div>
+  );
+};
+
+export default ThingGroup;
