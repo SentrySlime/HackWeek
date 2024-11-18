d00e8e388ce0f2650b3fcef1700e0c33eab971d6
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-11-18 21:43:22 +0100

    Created Post class entity and created PostRepository

---
+++ backend/src/main/java/io/sebbe/backend/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package io.sebbe.backend.repository;
+
+import io.sebbe.backend.model.Post;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface PostRepository extends ListCrudRepository<Post, > {
+}
