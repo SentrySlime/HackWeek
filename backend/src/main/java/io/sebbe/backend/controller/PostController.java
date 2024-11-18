e68706c4d5fcf1a78371c73cfbacf2955083db95
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-11-18 21:32:32 +0100

    Created get method in PostController with mocked data

--- backend/src/main/java/io/sebbe/backend/controller/PostController.java
+++ backend/src/main/java/io/sebbe/backend/controller/PostController.java
@@ -1,10 +1,19 @@
 package io.sebbe.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class PostController {
+
+  @GetMapping
+  public ResponseEntity<String> getPost(){
+    return ResponseEntity.ok("hey");
+  }
+
+  
 
 }
